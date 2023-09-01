a450c3e0ed4634f0dad5d02ff047cb1fe617ccde
jeston13 <imjeston@gmail.com>
2023-09-01 15:56:01 +0530

    added add_to_cart workflow

--- src/test/java/starter/acceptancetests/WhenExer3.java
+++ src/test/java/starter/acceptancetests/WhenExer3.java
@@ -1,14 +1,45 @@
 package starter.acceptancetests;
-
-import net.serenitybdd.screenplay.Actor;
-import net.serenitybdd.screenplay.annotations.CastMember;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.annotations.CastMember;
+import net.serenitybdd.screenplay.ui.Button;
+import net.serenitybdd.screenplay.ui.Link;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import starter.actions.Forminput.EnterData;
+import starter.actions.navigation.NavigatTo;
+
 
 @ExtendWith(SerenityJUnit5Extension.class)
-public class WhenExer3 {
+class WhenExer3 {
 
     @CastMember
     Actor actor;
+
+    @Test
+    @DisplayName("Should be able Open The Login and add to cart")
+    void Add_to_cart_workflow() {
+        actor.attemptsTo(
+                NavigatTo.theSearchHomePage(),
+                EnterData.CLickELement(By.cssSelector("a[href='https://lifechangers.org/my-account/'")),
+                EnterData.forminput("pehohi7311@poverts.com",By.id("username")),
+                EnterData.forminput("pehohi7311@poverts.com@10",By.id("password")),
+                EnterData.CLickELement(By.cssSelector("input[value='Login']")),
+                //WaitUntil.the(Target.the("Resources Link").located(By.linkText("Resources")), isClickable()),
+                //Click.on(Target.the("Resources Link").located(By.linkText("Resources")))
+                //EnterData.CLickELement(new By.ByPartialLinkText("resources"))
+                Click.on(Link.containing("resources")),
+                Click.on(Button.withText("GO TO RESOURCE")),
+                Click.on(Button.withText("add to cart")),
+                EnterData.CLickELement(By.className("btn-cart"))
+
+        );
+
+
+    }
+
 
 }
