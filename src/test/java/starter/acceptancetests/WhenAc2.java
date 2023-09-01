7668c2b02e5c8ab09903b8ca2a77366fa754848c
zeus11011 <naikanish13@gmail.com>
2023-09-01 10:46:37 +0530

    refarctored code

---
+++ src/test/java/starter/acceptancetests/WhenAc2.java
@@ -0,0 +1,35 @@
+package starter.acceptancetests;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.actions.Evaluate;
+import net.serenitybdd.screenplay.annotations.CastMember;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import starter.actions.Forminput.EnterData;
+import starter.actions.navigation.NavigatTo;
+
+public class WhenAc2 {
+
+    @CastMember
+    Actor actor;
+    @Test
+    @DisplayName("Should be able Open The Login")
+    void Login() {
+        actor.attemptsTo(
+                NavigatTo.theSearchHomePage(),
+
+                EnterData.CLickELement(By.id("login2")),
+                EnterData.forminput("pehohi7311@poverts.com",By.id("loginusername")),
+                EnterData.forminput("pehohi7311@poverts.com",By.id("loginpassword")),
+
+                Evaluate.javascript("logIn()")
+//                EnterData.CLickELement(By.id("logout2"))
+                // Find(By.id("nameofuser"))
+//                Click.on(Button.withText("logout"))
+                // Click.on(By.cssSelector("css=button[value='Login in'"))
+        );
+
+
+    }
+}
